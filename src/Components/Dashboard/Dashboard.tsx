c61e0b8cd12e391b1996950635428ed1a1917200
okok0415 <dlawjdals11@naver.com>
2022-06-02 22:07:36 +0900

    220602

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,4 +1,20 @@
+import styled from "styled-components";
+
+const ProjectTitle = styled.div`
+    margin: 30px 0px;
+    padding-bottom: 10px;
+    font-size: 20px;
+    font-weight: 700;
+    border-bottom: 1px solid rgb(200,200,200);
+    display: grid;
+    grid-template-columns: 5fr 1fr;
+    align-items: center;
+`
+
 function Dashboard() {
+
+
+
     return <h1>Price</h1>;
 }
 
